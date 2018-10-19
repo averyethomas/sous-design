259aed2b0dc10840c969c4b716d7d37b5fbc0443
Avery Thomas <averyethomas@gmail.com>
2018-10-18 22:37:23 -0500

    launch updates

--- _assets/scripts.js
+++ _assets/scripts.js
@@ -1,80 +1,55 @@
-var folder = '_assets/images/backgrounds/';
+var backgrounds = [ ];
 
-var backgrounds = [
-	'sous-background-01.jpg',
-	'sous-background-02.jpg',
-	'sous-background-03.jpg',
-	'sous-background-04.jpg',
-	'sous-background-05.jpg',
-	'sous-background-06.jpg',
-	'sous-background-07.jpg',
-	'sous-background-08.jpg',
-	'sous-background-09.jpg',
-];
+var num = 0;
 
-function getRandomNum(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
+var changeBackground = function(){
+		
+	var backgroundImageDivs = document.getElementsByClassName('background-image');
+	
+	for(var i = 0; i < backgrounds.length; i++) {
+		backgroundImageDivs[i].classList.remove('active');
+	}
+	
+	if(num >= (backgrounds.length - 1)) {
+		num = 0;
+	} else {
+		num += 1;
+	}
+	
+	backgroundImageDivs[num].className += ' active';
+};
+	
 var addBackgrounds = function(){
 	for(var i = 0; i < backgrounds.length; i++) {
 		$('.background').append('<div class="background-image" style="background-image: url(_assets/images/backgrounds/' + backgrounds[i] + ');"></div>')
 	}
 }
 
-addBackgrounds();
+$(document).ready(function() { 	
+	var folder = '_assets/images/backgrounds/';
 
-var changeBackground = function(){
+	$.ajax({
+	    url : folder,
+	    xhrFields: {
+	    	withCredentials: true
+	    },
+		success: function (data) {
+	        $(data).find("a").attr("href", function (i, val) {
+		        if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+		            backgrounds.push(val);
+		        } 
+		    });
+		    
+			addBackgrounds();
+			
+			//changeBackground();
+			
+			setInterval(changeBackground, 5000);
+	    }
+	});
 	
-	var backgroundImageDivs = document.getElementsByClassName('background-image');
-		
-	var num = Math.floor(getRandomNum(0, backgroundImageDivs.length));
 	
-	for(var i = 0; i < backgrounds.length; i++) {
-		backgroundImageDivs[i].classList.remove('active');
-	}
 	
-	backgroundImageDivs[num].className += ' active';
 	
-	console.log('run');
-};
-
-changeBackground();
-
-setInterval(changeBackground, 6000);
-
-
-
-
-//
-/*
-
-$.ajax({
-	url: folder  
-})
-.done(function( data ) {      
-   console.log(data);
+	
 });
-*/
-
-/*
-$.ajax({
-    url : folder,
-    xhrFields: {
-    	withCredentials: true
-    },
-	success: function (data) {
-        console.log(data);
-    }
-	    
-/*
-        $(data).find("a").attr("href", function (i, val) {
-            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
-                console.log(folder + val);
-            } 
-        });
-
-});
-*/
-
-// console.log(folder);
